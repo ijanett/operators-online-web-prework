a7d7395369ad1325f6b60d8d1ea9d55fbafa8286
Irma J Mejia <ijanettmejia@outlook.com>
2019-05-01 04:17:18 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,12 @@
 
 def unsafe?(speed)
   if speed == < 40 || > 60
-    return "true"
+    return true
   else speed == > 40 && < 60
-     return "false"
+     return false
   end
 end
 
 def not_safe?(speed)
-  40>speed>60 ? "true" : "false"
+  40>speed>60 ? true : false
 end
