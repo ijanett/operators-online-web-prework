c29955d53f74f7b6263908e66c7d75cb450b37c2
Irma J Mejia <ijanettmejia@outlook.com>
2019-05-01 04:20:58 +0000

    Automatically backed up by Learn

--- spec/operations_spec.rb
+++ spec/operations_spec.rb
@@ -1,4 +1,3 @@
-require_relative ./spec_helper.#!/usr/bin/env ruby -wKU
 
 describe "Operations" do
   let(:file_contents) { File.read(File.join(File.dirname(__FILE__), '..', 'lib/operations.rb')) }
